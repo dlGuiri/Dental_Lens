8447b9da20e0e5725298c0fd333494a7d7e30a3f
Darwin Guirigay <darwinlouis4@gmail.com>
2025-08-26 17:59:13 +0800

    Removed Unnecessary Files: hello.ts and test-middleware.ts

--- code/teethanalyzer-application/pages/api/test-middleware.ts
+++
@@ -1,9 +0,0 @@
-import type {NextApiRequest, NextApiResponse} from "next";
-import dbConnect from "middleware/db-connect";
-import {findAllLocations} from "@/mongoose/users/services";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-    await dbConnect();
-    const locations = await findAllLocations();
-    res.status(200).json(locations);
-}
